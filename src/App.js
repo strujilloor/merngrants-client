61f5f3a91b0b790c9e59c11a0e6e705015c63270
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-04 21:27:28 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Grants from './components/grants/Grants';
 function App() {
   return (
-    <h1>Mern Grants</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/grants" /> 
+        </Route> 
+        <Route exact path="/grants" component={Grants} /> 
+        {/* <Route exact path="/grant:id" component={Grant} />  */}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/grants/Grants.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grants = () => {
+    return (
+        <h1>
+            Desde Grants
+        </h1>
+    );
+};
+
+export default Grants;
