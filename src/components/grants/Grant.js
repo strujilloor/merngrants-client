7ea2909de6cf2fa22031dbb237dfa2e9a7f40840
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-05 15:09:48 -0500

    Grants List

--- src/components/grants/Grant.js
+++ src/components/grants/Grant.js
@@ -1,12 +1,43 @@
 import React from 'react';
 
 const Grant = ({ grant }) => {
+    const od = new Date(grant.openDate);
+    const cd = new Date(grant.closeDate);
+    
     return (
-        <li>
-            <button
-                type="button"
-                className="btn btn-blank"
-            >{grant.title} </button>
+        <li className="tarea">
+            <p>{grant.title} </p>
+
+            <div className="estado">
+                {grant.estado 
+                ?  
+                    (
+                        <button
+                            type="button"
+                            className="completo"
+                        >Completo</button>
+                    )
+                : 
+                    (
+                        <button
+                            type="button"
+                            className="incompleto"
+                        >{grant.oppStatus}</button>
+                    )
+                }
+            </div>
+
+            <div className="acciones">
+                <button 
+                    type="button"
+                    className="btn btn-primario"
+                >{od.getFullYear()}</button>
+
+                <button
+                    type="button"
+                    className="btn btn-secundario"
+                >{cd.getFullYear()}</button>
+            </div>
         </li>
     );
 };
--- src/components/grants/GrantsList.js
+++ src/components/grants/GrantsList.js
@@ -4,9 +4,11 @@ import Grant from './Grant';
 const GrantsList = () => {
 
     const grants = [
-        {title: 'Leaders for Change-Virtual Exchange Program'},
-        {title: 'Increasing Public Awareness and Provider Education About Primary Immun...'},
-        {title: 'Mitigating Violent Extremism in Masaka Region'}
+        {title: 'Leaders for Change-Virtual Exchange Program', number: 'S-DR860-20-NOFO-005', agencyCode: 'DOS-DOM', oppStatus: 'posted', openDate: '2020-06-05T00:00:00.000+00:00', closeDate: '2020-07-07T00:00:00.000+00:00'},
+
+        {title: 'Increasing Public Awareness and Provider Education About Primary Immun...',  number: 'CDC-RFA-OE17-17020401SUPP20', agencyCode: 'HHS-CDC-CSELS', oppStatus: 'posted', openDate: '2020-06-05T00:00:00.000+00:00', closeDate: '2020-07-07T00:00:00.000+00:00'},
+
+        {title: 'Mitigating Violent Extremism in Masaka Region',  number: 'DOS-UG-PAS-FY20-1', agencyCode: 'DOS-UGA', oppStatus: 'posted', openDate: '2020-06-05T00:00:00.000+00:00', closeDate: '2020-07-07T00:00:00.000+00:00'}
     ]
 
     return (
@@ -23,6 +25,8 @@ const GrantsList = () => {
                     ))
                 }
             </ul>
+
+            {/* PAGINATION HERE */}
         </Fragment>
     );
 };
